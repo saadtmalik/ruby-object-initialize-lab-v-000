a54b4fc6311c1c3b51da6216553efe58dda78ccb
Saad Malik <saad.t.malik@gmail.com>
2018-11-03 19:56:18 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initialize(name)
+    @name = name
+  end
+  def name= (name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
+
+class Dog
+  def initialize (breed)
+    @breed = breed
+  end
+  def breed= (breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+end
