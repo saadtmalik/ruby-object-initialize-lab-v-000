db4d188eb8dc9a1403bd830185c60b2401b93509
Saad Malik <saad.t.malik@gmail.com>
2018-11-03 20:06:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def initialize (name,breed)
+  def initialize (name,breed="Mutt")
     @name = name
     @breed = breed
   end
-  def name= (name_breed="Mutt")
+  def name= (name_breed)
     name, breed = name_breed.split
     @name = name
     @breed = breed
